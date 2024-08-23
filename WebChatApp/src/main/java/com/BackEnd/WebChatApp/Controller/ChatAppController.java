c3a15332892ef6b2a6ee4a39c845c18cda77b94f
Mehemmed <yerivar0@gmail.com>
2024-08-23 23:16:11 +0400

    webchatapp

--- WebChatApp/src/main/java/com/BackEnd/WebChatApp/Controller/ChatAppController.java
+++ WebChatApp/src/main/java/com/BackEnd/WebChatApp/Controller/ChatAppController.java
@@ -6,8 +6,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
